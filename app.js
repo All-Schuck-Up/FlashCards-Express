c71080e214db43f6a509d7df064671523bae75fb
schuckrw <55608123+schuckrw@users.noreply.github.com>
2020-04-24 12:49:40 -0700

    Initial Commit

---
+++ app.js
@@ -0,0 +1,19 @@
+const express = require('express');
+const bodyParser = require('body-parser');
+const cookieParser = require('cookie-parser');
+
+const app = express();
+app.use(bodyParser.urlencoded({ extended: false }));
+app.use(cookieParser());
+
+app.set('view engine', 'pug');
+
+const mainRoutes = require('./routes');
+const cardRoutes = require('./routes/cards');
+
+app.use(mainRoutes);
+app.use('/cards', cardRoutes);
+
+app.listen(8080, () => {
+    console.log('App running on localhost:8080')
+});
