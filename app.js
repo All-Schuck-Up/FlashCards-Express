513b8068ae57c4fe3854d7fd2fc322aff75a63e5
schuckrw <55608123+schuckrw@users.noreply.github.com>
2020-04-24 13:26:30 -0700

    Update with Comments

--- app.js
+++ app.js
@@ -1,19 +1,24 @@
+// Init dependenies
 const express = require('express');
 const bodyParser = require('body-parser');
 const cookieParser = require('cookie-parser');
 
+// Init Middleware (app.use--indicates middleware functionality)
 const app = express();
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(cookieParser());
 
+// Brings in pug for simple easy to handle html handling for rendering
 app.set('view engine', 'pug');
 
+// Brings in our app routes useing .Router() from routes folder
 const mainRoutes = require('./routes');
 const cardRoutes = require('./routes/cards');
 
 app.use(mainRoutes);
 app.use('/cards', cardRoutes);
 
+// Runs server on port 8080 and displays msg to console
 app.listen(8080, () => {
     console.log('App running on localhost:8080')
 });
--- routes/cards.js
+++ routes/cards.js
@@ -1,15 +1,23 @@
+// Cards route
 const express = require('express');
 const router = express.Router();
-const { data } = require('../data/flashcardData.json');
-const { cards } = data;
+// Bring in flashCard data from the data json file in data folder
+const { data } = require('../data/flashcardData.json'); // This syntax is the same as "flashcardData.json.data"
+const { cards } = data; // This is the same as "data.cards"
 
+// Card route that follows '/cards/ (card id- for specific card)' TEST WITH: => http://localhost:8080/cards/0?side=question
 router.get('/:id', (req, res) => {
-    const { side } = req.query;
-    const { id } = req.params;
-    const text = cards[id][side];
+    const { side } = req.query; // This is a query for data for specific side of card (question/ answer)
+    const { id } = req.params; // Grabs data from flashcardData.json for card_id
+    const text = cards[id][side]; // Init var which is called in pug file card.pug to render text
     const { hint } = cards[id];
 
-    const templateData = { text, hint };
+    const templateData = { text }; // Init var which is called in pug file card.pug
+
+    // This makes app only display hint to user if on the question side of card
+    if (side === 'question') {
+        templateData.hint = hint;
+    }
 
     res.render('card', templateData);
 });
