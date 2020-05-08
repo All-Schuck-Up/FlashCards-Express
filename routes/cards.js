4c9233a22d0c1818c94c17e6a40de34eb59ab601
All-Schuck-Up <55608123+All-Schuck-Up@users.noreply.github.com>
2020-05-08 09:41:30 -0700

    display random card feature

--- routes/cards.js
+++ routes/cards.js
@@ -7,19 +7,24 @@ const { cards } = data; // This is the same as "data.cards"
 
 // Card route that follows '/cards/ (card id- for specific card)' TEST WITH: => http://localhost:8080/cards/0?side=question
 router.get('/:id', (req, res) => {
-    const { side } = req.query; // This is a query for data for specific side of card (question/ answer)
-    const { id } = req.params; // Grabs data from flashcardData.json for card_id
-    const text = cards[id][side]; // Init var which is called in pug file card.pug to render text
-    const { hint } = cards[id];
+  const { side } = req.query; // This is a query for data for specific side of card (question/ answer)
+  const { id } = req.params; // Grabs data from flashcardData.json for card_id
+  const text = cards[id][side]; // Init var which is called in pug file card.pug to render text
+  const { hint } = cards[id];
 
-    const templateData = { text }; // Init var which is called in pug file card.pug
+  const templateData = { text }; // Init var which is called in pug file card.pug
 
-    // This makes app only display hint to user if on the question side of card
-    if (side === 'question') {
-        templateData.hint = hint;
-    }
+  // This makes app only display hint to user if on the question side of card
+  if (side === 'question') {
+    templateData.hint = hint;
+    templateData.sideToShow = 'answer';
+    templateData.sideToShowDisplay = 'Answer';
+  } else if (side === 'answer') {
+    templateData.sideToSohow = 'question';
+    templateData.sideToShowDisplay = 'Question';
+  }
 
-    res.render('card', templateData);
+  res.render('card', templateData);
 });
 
 module.exports = router;
