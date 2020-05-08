f85548c6e902e247c2a44b8cc1240c70fd3be119
All-Schuck-Up <55608123+All-Schuck-Up@users.noreply.github.com>
2020-05-08 10:14:25 -0700

    initial commit

--- routes/cards.js
+++ routes/cards.js
@@ -5,14 +5,25 @@ const router = express.Router();
 const { data } = require('../data/flashcardData.json'); // This syntax is the same as "flashcardData.json.data"
 const { cards } = data; // This is the same as "data.cards"
 
+router.get('/', (req, res) => {
+  const numberOfCards = cards.length;
+  const flashcardId = Math.floor(Math.random() * numberOfCards); // generates random number/ id
+  res.redirect(`/cards/${flashcardId}?side=question`);
+});
+
 // Card route that follows '/cards/ (card id- for specific card)' TEST WITH: => http://localhost:8080/cards/0?side=question
 router.get('/:id', (req, res) => {
   const { side } = req.query; // This is a query for data for specific side of card (question/ answer)
   const { id } = req.params; // Grabs data from flashcardData.json for card_id
+
+  if (!side) {
+    res.redirect(`/cards/${id}?side=question`);
+  }
+  const name = req.cookies.username;
   const text = cards[id][side]; // Init var which is called in pug file card.pug to render text
   const { hint } = cards[id];
 
-  const templateData = { text }; // Init var which is called in pug file card.pug
+  const templateData = { id, text, name }; // Init var which is called in pug file card.pug
 
   // This makes app only display hint to user if on the question side of card
   if (side === 'question') {
--- routes/index.js
+++ routes/index.js
@@ -3,25 +3,27 @@ const router = express.Router();
 
 // Root route TEST WITH => http://localhost:8080
 router.get('/', (req, res) => {
-    const name = req.cookies.username;
-    if (name) { res.render('index', { name }) } else {
-        res.redirect('/hello');
-    }
+  const name = req.cookies.username;
+  if (name) {
+    res.render('index', { name });
+  } else {
+    res.redirect('/hello');
+  }
 });
 
 // Landing route TEST WITH => http://localhost:8080/hello (user should always land here unless not signed in)
 router.get('/hello', (req, res) => {
-    res.render('hello');
+  res.render('hello');
 });
 router.post('/hello', (req, res) => {
-    res.cookie('username', req.body.username);
-    res.redirect('/');
+  res.cookie('username', req.body.username);
+  res.redirect('/');
 });
 
 // Log out route (not functioning yet) To do:  code logout button to clear cookie and redirect to landing page
 router.post('/goodbye', (req, res) => {
-    res.clearCookies('username');
-    res.redirect('/hello');
+  res.clearCookie('username');
+  res.redirect('/hello');
 });
 
 module.exports = router;
